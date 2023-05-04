1169d9ed8e34e12c9d8fad1d59239b59c2f7b115
Eduarda <eduarda.knevitz@hotmail.com>
2023-05-04 01:56:07 -0300

    Estilizacao com o SASS

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,3 +1,9 @@
+import styles from "../styles/home.module.scss";
+
 export default function Home() {
-  return <h1>Hello Dev!</h1>;
+  return (
+    <h1 className={styles.title}>
+      Hello <span>Dev!</span>
+    </h1>
+  );
 }
