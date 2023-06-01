4489177c81c4e7268cc55ecb0b3be17db28a44c0
Eduarda <eduarda.knevitz@hotmail.com>
2023-06-01 08:29:36 -0300

    Client Side Rendering

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,9 +1,29 @@
-import styles from "../styles/home.module.scss";
+import { useEffect, useState } from "react";
+
+interface Post {
+  id: string;
+  title: string;
+}
 
 export default function Home() {
+  const [posts, setPosts] = useState<Post[]>([]);
+
+  useEffect(() => {
+    fetch("http://localhost:3000/posts").then((response) => {
+      response.json().then((data) => {
+        setPosts(data);
+      });
+    });
+  }, []);
+
   return (
-    <h1>
-      Hello <span>Dev!</span>
-    </h1>
+    <div>
+      <h1>Posts</h1>
+      <ul>
+        {posts.map((post) => (
+          <li key={post.id}>{post.title}</li>
+        ))}
+      </ul>
+    </div>
   );
 }
